ed5936e9c3f534392ae5ef032dc8baa9b91fe9cd
Thái Nguyễn Quốc <20520304@ms.uit.edu.vn>
2022-04-27 16:12:20 +0700

    insert DTO, DAO

---
+++ DTO/GOITAP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class GOITAP
+    {
+        private char magoi;
+        private char tengoi;
+        private char giamoithang;
+        private char coPT;
+
+        public char Magoi { get => magoi; set => magoi = value; }
+        public char Tengoi { get => tengoi; set => tengoi = value; }
+        public char Giamoithang { get => giamoithang; set => giamoithang = value; }
+    }
+}
---
+++ DTO/HOIVIEN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class HOIVIEN
+    {
+        private char mahv;
+        private char hoten;
+        private char phai;
+        private char ngsinh;
+        private char ngdangki;
+        private char sdt;
+        private char nvquanli;
+
+
+
+        public char Mahv { get => mahv; set => mahv = value; }
+        public char Hoten { get => hoten; set => hoten = value; }
+        public char Phai { get => phai; set => phai = value; }
+        public char Ngsinh { get => ngsinh; set => ngsinh = value; }
+        public char Ngdangki { get => ngdangki; set => ngdangki = value; }
+        public char Sdt { get => sdt; set => sdt = value; }
+        public char Nvquanli { get => nvquanli; set => nvquanli = value; }
+    }
+}
