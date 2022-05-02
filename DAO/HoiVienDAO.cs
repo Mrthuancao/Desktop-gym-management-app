d9f864c4ab79c3b9e3f49d28ea0242a1db406f6d
Thái Nguyễn Quốc <20520304@ms.uit.edu.vn>
2022-05-02 14:45:14 +0700

    Cập nhật DTO; thêm HoiVienDAO, HoiVienBUS

---
+++ DAO/HoiVienDAO.cs
@@ -0,0 +1,76 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class HoiVienDAO
+    {
+        public DataTable GetAllHoiVien()
+        {
+            string query = "SELECT * FROM HOIVIEN";
+            DBConnect db = new DBConnect();
+            DataTable dt = db.ExecuteQuery(query);
+            return dt;
+        }
+
+        public HOIVIEN GetHoivienByID(int idDrink)
+        {
+            string query = "SELECT * FROM HOIVIEN WHERE mahv = @mahv";
+            object[] value = new object[] { idDrink };
+            DBConnect db = new DBConnect();
+            DataTable dt = db.ExecuteQuery(query, value);
+            HOIVIEN drink = new HOIVIEN(dt.Rows[0]);
+            return drink;
+        }
+
+        public HOIVIEN GetHoivienByName(string name)
+        {
+            string query = "SELECT * FROM HOIVIEN WHERE hoten = @hoten";
+            object[] value = new object[] { name };
+            DBConnect db = new DBConnect();
+            DataTable dt = db.ExecuteQuery(query, value);
+            HOIVIEN drink = new HOIVIEN(dt.Rows[0]);
+            return drink;
+        }
+
+        
+
+        public DataTable GetAllHoivienDetailed()
+        {
+            string query = "SELECT HOIVIEN.mahv as Mã Hội viên, HOIVIEN.hoten as Họ tên, HOIVIEN.phai as Giới tính, HOIVIEN.ngsinh as Ngày sinh, HOIVIEN.ngdangki as Ngày đăng kí, HOIVIEN.sdt as SDT, HOIVIEN.nvquanli as Mã nhân viên PT";
+            DBConnect db = new DBConnect();
+            DataTable dt = db.ExecuteQuery(query);
+            return dt;
+        }
+
+        public bool InsertHoivien(string mahv, string hoten, string phai, string ngsinh, string ngdangki, string sdt, string nvquanli)
+        {
+            string query = "insert into HOIVIEN(mahv, hoten, phai, ngsinh, ngdangki, sdt, nvquanli) values(@mahv, @hoten, @phai, @ngsinh, @ngdangki, @sdt, @nvquanli)";
+            object[] value = new object[] { mahv, hoten, phai, ngsinh, ngdangki, sdt, nvquanli };
+            DBConnect db = new DBConnect();
+            return ((db.ExecuteNonQuery(query, value)) > 0);
+        }
+
+        public bool DeleteHoivien(string mahv)
+        {
+            string query = "delete from HOIVIEN where mahv = @mahv";
+            object[] value = new object[] { mahv };
+            DBConnect db = new DBConnect();
+            return ((db.ExecuteNonQuery(query, value)) > 0);
+        }
+
+        public bool UpdateHoivien(string mahv, string hoten, string phai, string ngsinh, string ngdangki, string sdt, string nvquanli)
+        {
+            string query = "update HOIVIEN set hoten = @hoten, phai = @phai, ngsinh = @ngsinh, ngdangki = @ngdangki, sdt = @sdt, nvquanli = @nvquanli where mahv = @mahv";
+            object[] value = new object[] { hoten, phai, ngsinh, ngdangki, sdt, nvquanli, mahv};
+            DBConnect db = new DBConnect();
+            return ((db.ExecuteNonQuery(query, value)) > 0);
+        }
+    }
+
+}
--- DTO/HOIVIEN.cs
+++ DTO/HOIVIEN.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class HOIVIEN
+    public class HOIVIEN
     {
-        private char mahv;
-        private char hoten;
-        private char phai;
-        private char ngsinh;
-        private char ngdangki;
-        private char sdt;
-        private char nvquanli;
+        private string mahv;
+        private string hoten;
+        private string phai;
+        private string ngsinh;
+        private string ngdangki;
+        private string sdt;
+        private string nvquanli;
 
-        public char Mahv { get => mahv; set => mahv = value; }
-        public char Hoten { get => hoten; set => hoten = value; }
-        public char Phai { get => phai; set => phai = value; }
-        public char Ngsinh { get => ngsinh; set => ngsinh = value; }
-        public char Ngdangki { get => ngdangki; set => ngdangki = value; }
-        public char Sdt { get => sdt; set => sdt = value; }
-        public char Nvquanli { get => nvquanli; set => nvquanli = value; }
+        public string Mahv { get => mahv; set => mahv = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public string Phai { get => phai; set => phai = value; }
+        public string Ngsinh { get => ngsinh; set => ngsinh = value; }
+        public string Ngdangki { get => ngdangki; set => ngdangki = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+        public string Nvquanli { get => nvquanli; set => nvquanli = value; }
 
-        public HOIVIEN(char mahv, char hoten, char phai, char ngsinh, char ngdangki, char sdt, char nvquanli)
+        public HOIVIEN(string mahv, string hoten, string phai, string ngsinh, string ngdangki, string sdt, string nvquanli)
         {
             this.Mahv = mahv;
             this.Hoten = hoten;
@@ -37,7 +37,14 @@ namespace DTO
         }
         public HOIVIEN(DataRow row)
         {
-            this.Mahv = (char)row["mahv"];
+            this.Mahv = row["mahv"].ToString();
+            this.Hoten = row["hoten"].ToString();
+            this.Phai = row["phai"].ToString();
+            this.Ngsinh = row["ngsinh"].ToString();
+            this.Ngdangki = row["ngdangki"].ToString();
+            this.Sdt = row["sdt"].ToString();
+            this.Nvquanli = row["nvquanli"].ToString();
+
         }
     }
 }
